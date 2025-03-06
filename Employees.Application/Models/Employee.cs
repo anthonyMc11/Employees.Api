227af5d10746b85b83aedb386e067e2e933ddf86
antmc <antmc11@gmail.com>
2025-03-06 09:54:56 +0000

    us-1: Adding employee entity model and backing database for the Repository

---
+++ Employees.Application/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace Employees.Application.Models;
+
+public class Employee
+{
+    public required Guid Id { get; init; }
+    public required string Title { get; init; }
+    public required string Firstname { get; init; }
+    public required string Surname { get; init; }
+    public required DateOnly DOB { get; init; }
+    public required string Gender { get; init; }
+    public required string Email { get; init; }
+    public required string Address { get; init; }
+}
--- Employees.Application/Repositories/EmployeeRepository.cs
+++ Employees.Application/Repositories/EmployeeRepository.cs
@@ -1,11 +1,34 @@
-﻿namespace Employees.Application.Repositories
+﻿using Employees.Application.Models;
+
+namespace Employees.Application.Repositories
 {
     public class EmployeeRepository : IEmployeeRepository
     {
+        private readonly List<Employee> _employees = [];
+
+        public Task<bool> CreateAsync(Employee employee)
+        {
+            _employees.Add(employee);
+            return Task.FromResult(true);
+        }
+
+        public Task<IEnumerable<Employee>> GetAllEmployeesAsync()
+        {
+            return Task.FromResult(_employees.AsEnumerable());
+        }
+
+        public Task<Employee?> GetEmployeeById(Guid id)
+        {
+            var employee = _employees.SingleOrDefault(x => x.Id == id);
+            return Task.FromResult(employee);
+        }
     }
 
     public interface IEmployeeRepository
     {
+        Task<bool> CreateAsync(Employee employee);
 
+        Task<Employee?> GetEmployeeById(Guid id);
+        Task<IEnumerable<Employee>> GetAllEmployeesAsync();
     }
 }
